616c30c2ef6dd82be294ca907e13539c91f30914
sandeep-chhabra <sandeepchhabra@Sakets-MacBook-Pro.local>
2017-10-06 20:58:38 +0530

    Fixed intermittent crash due to unequal length of addition strings

--- Multiplier/ViewController.swift
+++ Multiplier/ViewController.swift
@@ -15,47 +15,55 @@ enum Direction {
 }
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var num1TxtField: UITextField!
     
     @IBOutlet weak var num2TxtField: UITextField!
     
     @IBOutlet weak var resulTxtView: UITextView!
-   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBAction func multiplyNumbers(_ sender: Any) {
     
-      self.view.endEditing(true)
-      resulTxtView.text = multiplyStrings(string1: num1TxtField.text!, string2: num2TxtField.text!)
+    @IBAction func multiplyNumbers(_ sender: Any) {
+        
+        self.view.endEditing(true)
+        resulTxtView.text = multiplyStrings(string1: num1TxtField.text!, string2: num2TxtField.text!)
         
     }
-
+    
     
     func multiplyStrings(string1:String,string2:String) -> String {
         var resultArray:[String] = Array()
         
         var count = string2.characters.count - 1
-
+        
         var numberOfZeros = 0
+        
+        var maxLength = 0
         
         while count >= 0 {
             var res = multiply(string: string1, singleCharacter: string2[count])
             res = appendZeros(in: res, count: numberOfZeros, dir: .rear)//(count: numberOfZeros, string: res)
-            res = appendZeros(in: res, count: count, dir: .front)
+            //            res = appendZeros(in: res, count: count, dir: .front)
             resultArray.append(res)
             count -= 1
             numberOfZeros += 1
+            
+            maxLength = maxLength > res.length ? maxLength : res.length
         }
+        
         print("Addition Array = \(resultArray.description)")
+        resultArray = makeEqualLength(strings: resultArray, length: maxLength)
+        print("Addition Array after equal length = \(resultArray.description)")
+        
         return add(strings:resultArray )
     }
     
@@ -67,7 +75,7 @@ class ViewController: UIViewController {
         var resultstring = ""
         
         while count >= 0 {
-           
+            
             let num1 = string[count]
             let num2 = singleCharacter
             
@@ -87,9 +95,44 @@ class ViewController: UIViewController {
         return resultstring
     }
     
+
+    
+    func add(strings:[String]) -> String {
+        
+        
+        var resultStr = ""
+        var carry = 0
+        
+        var lenthCounter  = strings[0].length - 1
+        
+        while lenthCounter >= 0 {
+            var tempSum = 0
+            
+            for  count in  0 ... (strings.count - 1) {
+                let str = strings[count]
+                tempSum += Int( str[lenthCounter])!
+            }
+            
+            tempSum += carry
+            carry = tempSum/10
+            tempSum = tempSum%10
+            
+            resultStr.insert(Character.init("\(tempSum)") , at: resultStr.startIndex)
+            lenthCounter -= 1
+        }
+        
+        if carry != 0{
+            resultStr.insert(Character.init("\(carry)") , at: resultStr.startIndex)
+        }
+        
+        return resultStr
+        
+    }
+    
+  //MARK: - HELPER
     func appendZeros(in string:String, count:Int,dir:Direction) -> String {
         if var resultStr = String.init(string){
-            if count == 0{
+            if count <= 0{
                 return resultStr
             }
             dir == .rear ? resultStr.append("0") : resultStr.insert(Character.init("0"), at: resultStr.startIndex)
@@ -101,55 +144,31 @@ class ViewController: UIViewController {
         }
     }
     
-    func add(strings:[String]) -> String {
+    func makeEqualLength(strings:[String],length:Int) -> [String]{
+        var resultArr = [String]()
         
+        for str in strings{
+            resultArr.append(appendZeros(in: str, count: length - str.length, dir: .front))
+        }
         
-            var resultStr = ""
-            var carry = 0
-            
-            var lenthCounter  = strings[0].length - 1
-            
-            while lenthCounter >= 0 {
-                var tempSum = 0
-                
-                for  count in  0 ... (strings.count - 1) {
-                    let str = strings[count]
-                    tempSum += Int( str[lenthCounter])!
-                }
-                
-                tempSum += carry
-                carry = tempSum/10
-                tempSum = tempSum%10
-                
-                resultStr.insert(Character.init("\(tempSum)") , at: resultStr.startIndex)
-                lenthCounter -= 1
-            }
-            
-            if carry != 0{
-                resultStr.insert(Character.init("\(carry)") , at: resultStr.startIndex)
-            }
-            
-            return resultStr
-        
+        return resultArr
     }
     
-    
-    
-//    func performMathOperation(type:operationType,num1:String,num2:String) -> (carry:String,val:String) {
-//       
-//        switch type {
-//        case .multiply:
-//            let res = Int(num1)! * Int(num2)!
-//            let carry = Int(res/10)
-//            let val = Int(res%10)
-//            return ("\(carry)","\(val)")
-//        case .add:
-//            let res = Int(num1)! + Int(num2)!
-//            let carry = Int(res/10)
-//            let val = Int(res%10)
-//            return ("\(carry)","\(val)")
-//        }
-//    }
-//    
+    //    func performMathOperation(type:operationType,num1:String,num2:String) -> (carry:String,val:String) {
+    //
+    //        switch type {
+    //        case .multiply:
+    //            let res = Int(num1)! * Int(num2)!
+    //            let carry = Int(res/10)
+    //            let val = Int(res%10)
+    //            return ("\(carry)","\(val)")
+    //        case .add:
+    //            let res = Int(num1)! + Int(num2)!
+    //            let carry = Int(res/10)
+    //            let val = Int(res%10)
+    //            return ("\(carry)","\(val)")
+    //        }
+    //    }
+    //    
 }
 
