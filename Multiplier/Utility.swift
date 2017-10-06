375f05eeb14c5b18bd235f6c1d8e1a2eca2f545f
sandeep-chhabra <sandeepchhabra@Sakets-MacBook-Pro.local>
2017-10-06 20:25:19 +0530

    Added all code

---
+++ Multiplier/Utility.swift
@@ -0,0 +1,37 @@
+//
+//  Utility.swift
+//  Multiplier
+//
+//  Created by Sandeep Chhabra on 06/10/17.
+//  Copyright © 2017 SandeepChhabra. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+    var length: Int {
+        return self.characters.count
+    }
+    
+    subscript (i: Int) -> String {
+        return self[i ..< i + 1]
+    }
+    
+    func substring(fromIndex: Int) -> String {
+        return self[min(fromIndex, length) ..< length]
+    }
+    
+    func substring(toIndex: Int) -> String {
+        return self[0 ..< max(0, toIndex)]
+    }
+    
+    subscript (r: Range<Int>) -> String {
+        let range = Range(uncheckedBounds: (lower: max(0, min(length, r.lowerBound)),
+                                            upper: min(length, max(0, r.upperBound))))
+        let start = index(startIndex, offsetBy: range.lowerBound)
+        let end = index(start, offsetBy: range.upperBound - range.lowerBound)
+        return String(self[start ..< end])
+    }
+    
+}
